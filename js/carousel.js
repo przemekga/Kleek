048613e0d133991961dd84e9462eb298b8385ecc
przemek <przemek@kleek.io>
2016-06-07 17:04:56 +0200

    slider

--- js/carousel.js
+++ js/carousel.js
@@ -1,7 +1,8 @@
-$('.owl-carousel').owlCarousel({
-	items : 10, //10 items above 1000px browser width
-  itemsDesktop : [1000,5], //5 items between 1000px and 901px
-  itemsDesktopSmall : [900,3], // betweem 900px and 601px
-  itemsTablet: [600,2], //2 items between 600 and 0
-  itemsMobile : false // itemsMobile disabled - inherit from itemsTablet option
+$(document).ready(function(){
+  $('.slider').slick({
+    appendDots: $('.slider'),
+    dots: true,
+    slidesToShow: 3,
+    arrows: false
+  });
 });
