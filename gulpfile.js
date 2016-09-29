492c8cf97e446f93d613a16594f5d11827f30bc5
Przemek Gajdas <przemekgajdas@gmail.com>
2016-09-29 11:29:04 +0200

    browsersync

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,22 @@
 var gulp = require('gulp'),
     sass = require('gulp-sass'),
-    livereload = require('gulp-livereload'),
-    webserver = require('gulp-webserver');
+    browserSync = require('browser-sync').create();
 
-gulp.task('webserver', function() {
-  gulp.src('')
-    .pipe(webserver({
-      livereload: true,
-      directoryListing: true,
-      open: true
-    }));
+gulp.task('serve', ['sass'], function() {
+
+    browserSync.init({
+        server: "./"
+    });
+
+    gulp.watch("scss/**/*.scss", ['sass']);
+    gulp.watch("*.html").on('change', browserSync.reload);
 });
- 
 
 gulp.task('sass', function() {
-  gulp.src('./scss/**/*.scss')
-    .pipe(sass())
-    .pipe(gulp.dest('css'))
-    .pipe(livereload());
-});
- 
-gulp.task('watch', function() {
-  livereload.listen();
-  gulp.watch('./scss/**/*.scss', ['sass']);
+    return gulp.src("scss/**/*.scss")
+        .pipe(sass())
+        .pipe(gulp.dest("css"))
+        .pipe(browserSync.stream());
 });
 
-gulp.task('default', ['sass', 'watch', 'webserver']);
+gulp.task('default', ['serve']);
